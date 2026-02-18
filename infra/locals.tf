6f3bcb8ff1824372816d311074f102955186b5d8
fernandes <contatomatheusfernandeeess@gmail.com>
2026-02-18 07:29:08 -0300

    fix pr

--- infra/locals.tf
+++ infra/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  lambda_sender_image_uri = "${aws_ecr_repository.lambda_sender_repo.repository_url}:${var.lambda_image_tag}"
+  lambda_sender_image_uri = "${aws_ecr_repository.lambda_sender_repo.repository_url}:${var.lambda_image_tag}" 
 }
